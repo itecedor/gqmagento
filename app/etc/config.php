8f44995415713bf4ae6b7032c3aa09f55b5d4d82
Ivete Tecedor <yivich@gmail.com>
2018-09-13 13:53:31 -0400

    added posim and taxjar extensions

--- app/etc/config.php
+++ app/etc/config.php
@@ -123,8 +123,11 @@ return [
         'Magento_CatalogWidget' => 1,
         'Dotdigitalgroup_Email' => 1,
         'Magento_WishlistAnalytics' => 1,
+        'POSIMWebExt_EVOLinkerAPI' => 1,
+        'POSIMWebExt_WebExtManager' => 1,
         'Shopial_Facebook' => 1,
         'Temando_Shipping' => 1,
+        'Taxjar_SalesTax' => 1,
         'Vertex_Tax' => 0
     ]
 ];
